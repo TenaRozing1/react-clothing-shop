1ad0a685831fcb80389b6a068e0f9b3059b49423
Tena Rozing <tena.rozing@gauss.dev>
2024-03-12 19:58:58 +0100

    Added reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,12 +1,12 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangeListener,
   signOutUser,
-  createUserDocumentFromAuth
+  createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
 
 // useContext je React hook koji omogućuje komponentama da pristupe vrijednostima koje su podijeljene unutar Contexta.
-// Context je mehanizam koji omogućuje prijenos podataka kroz cijelo stablo komponenata bez potrebe da se propisuju
+// Context je mehanizam koji omogućuje prijenos podataka kroz cijelo stablo komponenata bez potrebe da se prepisuju
 // te vrijednosti kroz svaku komponentu.
 
 //as the actual value you want to access
@@ -15,8 +15,36 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
+
+export const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+  }
+
   const value = { currentUser, setCurrentUser };
 
   signOutUser();
@@ -32,3 +60,13 @@ export const UserProvider = ({ children }) => {
   }, []);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/* 
+ 
+const userReducer = (state, action) => {
+  return {
+    currentUser: null
+  }
+}
+
+*/
